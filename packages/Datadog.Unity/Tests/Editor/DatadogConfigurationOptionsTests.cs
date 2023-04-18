123d2be8f23192e430131265a874ec0cf5a24a03
Jeff Ward <jeff.ward@datadoghq.com>
2023-04-18 17:24:22 -0400

    test: Add first set of tests for configuration options

---
+++ packages/Datadog.Unity/Tests/Editor/DatadogConfigurationOptionsTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+
+namespace Datadog.Unity.Editor.Tests
+{
+    public class DatadogConfigurationOptionsTests
+    {
+        private string _assetPath;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _assetPath = Path.Combine("Assets", Guid.NewGuid().ToString(), "DatadogSettings.asset");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AssetDatabase.DeleteAsset(Path.GetDirectoryName(_assetPath));
+        }
+
+        [Test]
+        public void GetOrCreateCreatesAssetCreatesAsset()
+        {
+            Assert.IsFalse(File.Exists(_assetPath));
+
+            DatadogConfigurationOptions.GetOrCreate(_assetPath);
+
+            Assert.IsTrue(File.Exists(_assetPath));
+        }
+
+        [Test]
+        public void GetOrCreateCreatesAssetCreatesOptionsWithProperDefaults()
+        {
+            Assert.IsFalse(File.Exists(_assetPath));
+
+            var options = DatadogConfigurationOptions.GetOrCreate(_assetPath);
+
+            Assert.IsTrue(options.Enabled);
+            Assert.IsEmpty(options.ClientToken);
+            Assert.AreEqual(options.Site, DatadogSite.us1);
+            Assert.AreEqual(options.DefaultLoggingLevel, LogType.Log);
+        }
+    }
+}
