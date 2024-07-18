7d09ad1e68803c9c45453f2ee0dd6447f5aa5ea8
Jeff Ward <jeff.ward@datadoghq.com>
2024-07-18 09:44:35 -0400

    Update samples/Demo Data/add-keys.py
    Co-authored-by: Nikita Ogorodnikov <4046447+0xnm@users.noreply.github.com>

--- samples/Demo Data/add-keys.py
+++ samples/Demo Data/add-keys.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 # -----------------------------------------------------------
 # Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
