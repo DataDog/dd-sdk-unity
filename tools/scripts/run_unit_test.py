1c47258d89a7de0542d8dc73897c0af5e3cce1f0
Jeff Ward <jeff.ward@datadoghq.com>
2025-05-02 11:22:23 -0400

    chore: Add 2021 test scaffold
    Modify the build to support a matrix of unit tests across scaffolds.
    
    refs: RUM-7957

--- tools/scripts/run_unit_test.py
+++ tools/scripts/run_unit_test.py
@@ -11,13 +11,14 @@ import asyncio
 import os
 import unity_helpers as uh
 
-integration_project_path = "../../samples/Datadog Sample"
+default_project_path = "../../samples/Datadog Sample"
 
 async def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--retry", default=0, help="The number of times to retry if a Unity License cannot be obtained")
     arg_parser.add_argument("--retry-wait", default=100, help="The amount of time to wait before retrying after a license failure")
     arg_parser.add_argument("--unity-version", default=uh.DEFAULT_UNITY_VERSION, help="What version of Unity to use. May be a partial version.")
+    arg_parser.add_argument("--project-path", default=default_project_path, help="The path of the project to run unit tests on.")
     args = arg_parser.parse_args()
 
     license_retry_count = args.retry
@@ -35,15 +36,16 @@ async def main():
             print("Failed to get floatling license on CI")
             return 1
 
+    project_path = args.project_path
 
     return_code = await uh.run_unity_command(license_retry_count, license_retry_wait,
-        "-runTests", "-batchMode", "-projectPath", f'"{integration_project_path}"',
+        "-runTests", "-batchMode", "-projectPath", f'"{project_path}"',
         "-testCategory", "!integration",
         "-testResults", "tmp/results.xml", "-logFile", "-",
     )
 
     return_code = await uh.run_unity_command(license_retry_count, license_retry_wait,
-        "-runTests", "-batchMode", "-projectPath", f'"{integration_project_path}"',
+        "-runTests", "-batchMode", "-projectPath", f'"{project_path}"',
         "-testCategory", "!integration", '-testPlatform', 'PlayMode',
         "-testResults", "tmp/results-play-mode.xml", "-logFile", "-",
     )
@@ -51,8 +53,8 @@ async def main():
     if token is not None:
         await uh.return_unity_license(token)
 
-    uh.transform_nunit_to_junit("../../samples/Datadog Sample/tmp/results.xml", "../../samples/Datadog Sample/tmp/junit-results.xml")
-    uh.transform_nunit_to_junit("../../samples/Datadog Sample/tmp/results-play-mode.xml", "../../samples/Datadog Sample/tmp/junit-results-play-mode.xml")
+    uh.transform_nunit_to_junit(f"{project_path}/tmp/results.xml", f"{project_path}/tmp/junit-results.xml")
+    uh.transform_nunit_to_junit(f"{project_path}/tmp/results-play-mode.xml", f"{project_path}/tmp/junit-results-play-mode.xml")
 
     return return_code
 
