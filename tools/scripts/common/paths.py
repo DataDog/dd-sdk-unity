a488de51f695e42a0b9ecf1b9eb0405a6b711063
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-06-27 18:53:13 -0400

    chore: More CI script refactoring, to make releases easier

--- tools/scripts/common/paths.py
+++
@@ -1,8 +0,0 @@
-import os
-
-__tools_scripts_common__ = os.path.dirname(__file__)
-__repo_root__ = os.path.abspath(os.path.join(__tools_scripts_common__, '..', '..', '..'))
-
-
-def repo_path(*args: str) -> str:
-    return os.path.normpath(os.path.join(__repo_root__, *args))
--- tools/scripts/common/types.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class ExternalDependencyVersions:
-    dd_sdk_android: str
-    dd_sdk_ios: str
