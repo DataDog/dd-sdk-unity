b1f7f6d1d87c40e57757a2a82c1793a5414f7883
Alex Forsythe <alex.forsythe@datadoghq.com>
2025-11-12 11:09:11 -0500

    chore(ci): Ignore spurious GraphQL errors from Unity Hub output
    This output started appearing after a recent Unity Hub upgrade; they are
    the result of Unity Hub constructing a GraphQL query in a way that
    Unity's own backend isn't happy with, then leaking that output.

--- tools/scripts/common/unity/install.py
+++ tools/scripts/common/unity/install.py
@@ -227,6 +227,14 @@ class UnityInstall:
     @classmethod
     def parse(cls, line: str) -> Optional['UnityInstall']:
         """Parses a line of output from Unity Hub's 'editors --installed' command."""
+        # Unity Hub prints a bunch of spurious GraphQL errors alongside the actual
+        # command output, so just ignore any such lines entirely:
+        for substr in ['GraphQL', 'UnityReleaseLabel', '//bit.ly/']:
+            if substr in line:
+                return None
+
+        # Treat any other lines matching this regex as valid output, and attempt to
+        # parse the Unity version and install path from them
         pattern = re.compile(r'^(\S+)\s+\(([^)]+)\),? installed at (.+)$')
         match = pattern.match(line)
         if match:
