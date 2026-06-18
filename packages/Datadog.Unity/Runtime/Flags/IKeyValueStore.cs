c047abb06b3602f082e72a255b55cd4602240583
typotter <tyler.potter@datadoghq.com>
2026-06-18 11:10:59 -0600

    feat(01-01): add IKeyValueStore, PlayerPrefsKeyValueStore, and IFlagsCacheWriter
    - IKeyValueStore: internal interface with GetString/SetString/DeleteKey/Save
    - PlayerPrefsKeyValueStore: expression-bodied wrapper delegating to UnityEngine.PlayerPrefs
    - IFlagsCacheWriter: internal interface with void Write(rawJson, context) seam

---
+++ packages/Datadog.Unity/Runtime/Flags/IKeyValueStore.cs
@@ -0,0 +1,14 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-Present Datadog, Inc.
+
+namespace Datadog.Unity.Flags
+{
+    internal interface IKeyValueStore
+    {
+        string GetString(string key, string defaultValue);
+        void SetString(string key, string value);
+        void DeleteKey(string key);
+        void Save();
+    }
+}
